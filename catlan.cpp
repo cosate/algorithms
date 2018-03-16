8b56993e996d13ddf819035b4640f921675746ca
gaofeijin <397830271@qq.com>
2018-03-16 10:07:50 +0800

    done

--- catlan.cpp
+++ catlan.cpp
@@ -3,35 +3,31 @@
 #include<vector>
 using namespace std;
 
-void catlan(vector<vector<int>>& res, vector<int> tmp, stack<int>& in, stack<int>& stk)
+void catlan(vector<vector<int>>& res, vector<int> tmp, vector<int>& in, stack<int>& stk)
 {
-	if(stk.empty())
+	if (stk.empty())
 	{
-		while(!in.empty())
-		{
-			tmp.push_back(in.top());
-			in.pop();
-		}
+		for (int i = in.size() - 1; i >= 0; i--)
+			tmp.push_back(in[i]);
 		res.push_back(tmp);
 	}
 	else
 	{
 		int k = stk.top();
-		in.push(k);
+		in.push_back(k);
 		stk.pop();
 		catlan(res, tmp, in, stk);
 		stk.push(k);
-		in.pop();
+		in.pop_back();
 
-
-		if(!in.empty())
+		if (!in.empty())
 		{
-			int t = in.top();
+			int t = in.back();
 			tmp.push_back(t);
-			in.pop();
+			in.pop_back();
 			catlan(res, tmp, in, stk);
 			tmp.pop_back();
-			in.push(t);
+			in.push_back(t);
 		}
 	}
 }
@@ -39,6 +35,12 @@ void catlan(vector<vector<int>>& res, vector<int> tmp, stack<int>& in, stack<int
 int main()
 {
 	stack<int> stk;
+	stk.push(11);
+	stk.push(10);
+	stk.push(9);
+	stk.push(8);
+	stk.push(7);
+	stk.push(6);
 	stk.push(5);
 	stk.push(4);
 	stk.push(3);
@@ -48,14 +50,15 @@ int main()
 
 	vector<vector<int>> res;
 	vector<int> tmp;
-	stack<int> in;
+	vector<int> in;
 	catlan(res, tmp, in, stk);
-	for(int i = 0; i < res.size(); i++)
+	cout << res.size() << endl;
+	/*for (int i = 0; i < res.size(); i++)
 	{
-		for(int j = 0; j < res[0].size(); j++)
-			cout<<res[i][j]<<"    ";
-		cout<<endl;
-	}
+		for (int j = 0; j < res[0].size(); j++)
+			cout << res[i][j] << "    ";
+		cout << endl;
+	}*/
 
 	return 0;
 }
